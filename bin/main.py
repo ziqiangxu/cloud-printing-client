4c32f132092a27ebd1aaefab7537155e45b50732
ziqiangxu <1401357890@qq.com>
2017-10-15 14:14:14 +0800

    fast-commit

--- bin/main.py
+++ bin/main.py
@@ -4,15 +4,15 @@ import getpass
 import json
 import os
 import sys
-bin_path = os.getcwd()[:-4]
-sys.path.append(bin_path)
-print(sys.path)
 import bin.printer as printer
 import re
 import win32api
 import time
 import threading
 import bin.receiver3 as receiver
+bin_path = os.getcwd()[:-4]
+sys.path.append(bin_path)
+print(sys.path)
 
 
 def get_json(json_path,json_name):
@@ -47,9 +47,12 @@ class ThreadReceiver(threading.Thread):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+
     def run(self):
         receiver.start()
         print("main:开始线程："+ self.name)
+
+
 receiver1 = ThreadReceiver(1, "receiver")
 receiver1.start()
 # 线程2：打印接收完毕的文件
@@ -114,6 +117,8 @@ class ThreadPrintReceived (threading.Thread):
     def run(self):
         print ("main:开始线程：" + self.name)
         print_received_files()
+
+
 print_thread1 = ThreadPrintReceived(1, "print_receive_files")
 print_thread1.start()
 
@@ -135,5 +140,7 @@ class ThreadPrintLocal (threading.Thread):
     def run(self):
         print ("main:开始线程：" + self.name)
         print_local_files()
+
+
 local_print_thread1 = ThreadPrintLocal(1, "print_local_files")
 local_print_thread1.start()
--- bin/test.py
+++ bin/test.py
@@ -1,40 +1,11 @@
-import os
-import json
-"""
-HOME_PATH = 'C:\\printer\\received'
-user = os.path.join(HOME_PATH, "18796282979")
-tasks = os.listdir(user)
-for task in tasks:
-    task_path = os.path.join(user, task)
-    file_list = os.listdir(task_path)
-    print(file_list)
-    print(task_path)
-    f = open(os.path.join(task_path, "info.json"), "r")
-    info = json.load(f)
-    print(info)
-    for file in file_list:
-        print(file)
-"""
-from ftplib import FTP
-import os
-import time
-HOME_PATH = '/home/cloud_printing/received_files/'
-LOCAL_PATH = 'C:\\printer\\received'
-ERROR_TIMES = 0
-ftp = FTP()
-timeout = 120
-port = 21
-ip = "45.76.222.90"
-print("test:开始连接ftp站点")
-try :
-    ftp.connect(ip, port=21, timeout=120)
-except TimeoutError:
-    print("test:连接ftp超时")
-ftp.login('vsftp', 'abc123cloud')
+from PyQt5.QtWidgets import *
+import sys
 
 
-def download(id, task):
-    url = HOME_PATH + id + "/" + task + "/"
-    print("test:", url)
-    print("test:", ftp.nlst(url))
-download("18796282979", "22759321")
+#class MyWindow(QWidget)
+app = QApplication(sys.argv)
+w1 = QWidget()
+w1.resize(800,400)
+w1.setWindowTitle("云打印")
+w1.show()
+sys.exit(app.exec_())
